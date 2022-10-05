5b3cec8bf3cec1d16bf89891825f70701dce0599
PaulHaze <dev@artemist.com.au>
2022-10-05 17:39:25 +1100

    add basic nav bar links

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { DashboardButton } from './DashboardButton';
+import { NavLinks } from './NavLinks';
 
 // targets phones as default
 // sm is tablet size
@@ -38,6 +39,7 @@ export function Header() {
     <HeaderContainer>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src="/icons/logo-large.svg" alt="Prolog logo" />
+      <NavLinks />
       <DashboardButton />
     </HeaderContainer>
   );
---
+++ components/header/NavLinks.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+const breakpoints = {
+  sm: '(min-width: 480px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  xl: '(min-width: 1280px)',
+};
+
+const NavBarLinks = styled.nav`
+  display: none;
+  @media ${breakpoints.md} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const StyledLink = styled.a`
+  color: ${(props) => props.theme.color.gray[500]};
+  text-decoration: none;
+  margin: 1rem;
+  font-size: 1rem;
+`;
+
+export function NavLinks() {
+  return (
+    <NavBarLinks>
+      <StyledLink href="/">Home</StyledLink>
+      <StyledLink href="/products">Products</StyledLink>
+      <StyledLink href="/documentation">Documentation</StyledLink>
+      <StyledLink href="/pricing">Pricing</StyledLink>
+    </NavBarLinks>
+  );
+}
