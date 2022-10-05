01e20d10d6bdb440cd40641fbf2a42e8587a8526
PaulHaze <dev@artemist.com.au>
2022-10-05 15:38:27 +1100

    fix: add theme prop for color in dash btn

--- components/header/DashboardButton.tsx
+++ components/header/DashboardButton.tsx
@@ -7,18 +7,18 @@ import { Routes } from '@config/routes';
 const DashboardLink = styled.div`
   width: 165px;
   height: 44px;
-  background: #7f56d9;
+  background: ${(props) => props.theme.color.primary[600]};
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid #7f56d9;
+  border: 1px solid ${(props) => props.theme.color.primary[600]};
   border-radius: 8px;
   box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
 
   a {
     color: white;
     text-decoration: none;
-    font-size: 14px;
+    font-size: 16px;
   }
 `;
 
